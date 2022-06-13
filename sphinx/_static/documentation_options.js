9d9a1f92dc7e1398719eb05315c2fe49d874d2b5
hra687261 <hra687261@gmail.com>
2022-06-13 13:58:21 +0000

    Deploying to gh-pages from  @ 5f9cd5a17db9672c76fa9211150a40f473a7b32e 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (fd9a1ed) (2022/02/11 19:13)',
+    VERSION: 'master (5f9cd5a) (2022/06/13 13:58)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
